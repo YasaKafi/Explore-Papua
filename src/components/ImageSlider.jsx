78fb2e0280ac5bef4d29cf532af69e4d389ea924
Nareswara05 <naresalhaq9@gmail.com>
2023-09-26 15:13:34 +0700

    feat [NR] add fixed navbar, add gallery slider

---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import imageAssets from '../utils/helpers';
+
+
+const ImageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow:7,
+    autoplay: true, 
+    autoplaySpeed: 2000,
+    arrows : false,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings :{
+          slidesToShow : 5,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings :{
+          slidesToShow : 3,
+        },
+      },
+    ]
+  };
+
+  return (
+    <div className="container  mx-auto">
+      <Slider {...settings}>
+      <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+        <div className="px-1">
+          <img src={imageAssets.imgMEurimoo} alt="Image 1" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default ImageSlider;
