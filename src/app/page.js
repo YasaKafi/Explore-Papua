0cb7c7d6704496c5697f234486b7ce6b52179d70
“altharaa” <aletheamaureen01@gmail.com>
2023-10-06 07:35:01 +0700

    feat [AM] add features

--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,6 @@ import React from "react";
 import Routes from "../routes/routes";
 import NavBar from "../components/NavBar";
 import ImageSlider from "@/components/ImageSlider";
-import KilasPapua from "@/components/KilasPapua"; 
-import CardExplore from "@/components/CardExplore"; 
-import Explore from "@/components/Explore";
-
-
 
 const rtl = true
 
@@ -16,12 +11,10 @@ export default function Home(){
 
     return(
         <body>
-            {/* <ImageSlider rtl={false}/>
+            <ImageSlider rtl={false}/>
             <ImageSlider rtl={true}/>
-            <ImageSlider rtl={false}/> */}
-            {/* <KilasPapua/> */}
-            <Explore/>
-
+            <ImageSlider rtl={false}/>
+            <h1>Home</h1>
         </body>
     );
 }
